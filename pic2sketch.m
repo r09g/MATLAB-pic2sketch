a1338dedd1ead36ae4199e19a14bb8f461bda99d
Raymond H. Yang <raymond.yang@mail.mcgill.ca>
2019-01-05 23:28:13 -0500

    Major update: implementation of vectorization, use of ordfilt2 function instead of nested for-loops, 90% less computing time required.

--- pic2sketch.m
+++ pic2sketch.m
@@ -8,60 +8,18 @@
 function imI = pic2sketch(filename)
 % Prompt user to enter input image filename and read-in image
 I = imread(filename);
+
 % Remove image colours and convert from 1 to 3 channels
 bwI = rgb2gray(I);
 bwI = cat(3, bwI, bwI, bwI); 
-
-% Curving & invert colors.
-% Iterate through pixels and use 40 as threshold
-% Set everything below 40 as 0 and scale 40~255 to 0~255
-invI = zeros(size(I,1),size(I,2),size(I,3));
-for i = 1:size(I,1)
-    for j = 1:size(I,2)
-        if I(i,j,1) <= 40
-            bwI(i,j,:) = zeros(1,1,3);
-        else
-            bwI(i,j,:) = double(bwI(i,j,:) - 40) / 215 * 255;
-        end
-        % Invert colors
-        invI(i,j,:) = 255 - bwI(i,j,:);
-    end
-end
-
-% Remove noise in image by finding the local (square region on each
-% layer) minimum pixel value and assigning all local pixels that min
-% value
-filterI = zeros(size(I,1),size(I,2),size(I,3));
-for i = 1:size(filterI,1)-3
-    for j = 1:size(filterI,2)-3
-        for dim = 1:size(filterI,3)
-            % min on local layer of pre-filtered image
-            localmin = min(min(invI(i:i+3,j:j+3,dim)));
-            % assign min to corresponding local square region on filtered
-            % image
-            filterI(i:i+3,j:j+3,dim) = ones(4,4) * double(localmin);
-        end
-    end
-end
-
+% Curve adjust
+curI = curving(bwI);
+% Invert
+invI = 255 - curI;
+% min filter
+filterI = filter(invI);
 % Color dodge blend mode
-% Lighter layer is the base and darker layer is the top
-blendedI = zeros(size(I,1),size(I,2),size(I,3));
-for i = 1:size(blendedI,1)
-    for j = 1:size(blendedI,2)
-        for dim = 1:size(blendedI,3)
-            if I(i,j,dim) == 255 
-                blendedI(i,j,dim) = 255;
-            else
-                blendedI(i,j,dim) = min(255,filterI(i,j,dim)/(1 - double(bwI(i,j,dim))/255));
-                % adjust darkness 
-                if blendedI(i,j,dim) <= 125
-                    blendedI(i,j,dim) = 125;
-                end
-            end
-        end
-    end
-end
+blendedI = cdblend(filterI,curI);
 
 % Colorize sketch by overlaying original photo on top and adjusting
 % transparency
@@ -72,8 +30,45 @@ axis off
 axis image
 hold on
 imcolor = image(I);
+hold off
 set(imcolor,'AlphaData',0.3);   % transparency
-
+ 
 % Export colorized sketch
 f = getframe(gcf);
 imI = f.cdata;  % 3 color channel image matrix
+end
+% Functions----------------------------------------------------------------
+
+% Curving.
+% Iterate through pixels and use 40 as threshold
+% Set everything below 40 as 0 and scale 40~255 to 0~255
+function [curI] = curving(bwI)
+    curI = zeros(size(bwI,1),size(bwI,2),size(bwI,3));
+    cond = (bwI <= 40);
+    curI(cond) = 0;
+    curI(~cond) = double(bwI(~cond) - 40) ./ 215 .* 255;
+end
+
+% Remove noise in image by finding the local (square region on each
+% layer) minimum pixel value and assigning all local pixels that min
+% value
+% ordfilt2 takes ~2.2 seconds while 3 nested for-loops takes ~21.5 seconds
+function [filterI] = filter(invI)
+    filterI = zeros(size(invI,1),size(invI,2),size(invI,3));
+    for dim = 1:size(filterI,3)
+        % a smaller ones matrix works for images with smaller resolution
+        % a bigger ones matrix works for images with bigger resolution 
+        filterI(:,:,dim) = ordfilt2(invI(:,:,dim),1,ones(10,10));
+    end
+end
+
+% Color dodge blend mode
+% Lighter layer is the base and darker layer is the top
+function blendedI = cdblend(filterI,curI)
+    blendedI = zeros(size(curI,1),size(curI,2),size(curI,3));
+    cond = (curI == 255 );
+    blendedI(cond) = 255;
+    blendedI(~cond) = min(255,filterI(~cond)./(1 - double(curI(~cond))./255));
+    % filter out dark points
+    blendedI(blendedI <= 125) = 125;
+end
